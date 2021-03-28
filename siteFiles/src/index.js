f6c26da873423ce3673c7b6ef25216936906f9d8
Andrew Chadwick <chadders13@googlemail.com>
2021-03-28 02:25:51 +0100

    Updated added webpack and moved ignored

---
+++ siteFiles/src/index.js
@@ -0,0 +1,12 @@
+var randomArray = require("arrayrandompicker");
+
+var input = [1,2,3,4,5,6,7];
+
+var output = randomArray.selectRandomArrayItem(input);
+
+console.log(output);
+
+function getRandomNumber()
+{
+return randomArray.selectRandomArrayItem(input);
+}
